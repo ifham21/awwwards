95b139a2adfc7f5791306302292c471042d02c0b
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-03 23:34:31 +0530

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awwards</h1>
+      <h1 className='text-5xl text-violet-800 font-bold '>Welcome to Awwards</h1>
     </main>
   )
 }
